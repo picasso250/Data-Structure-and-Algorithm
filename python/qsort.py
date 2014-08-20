6c5b4a21bdb7d689d21a2371966074c4652c8907
Ryan <cumt.xiaochi@gmail.com>
2014-08-20 16:19:13 +0800

    z

--- python/qsort.py
+++ python/qsort.py
@@ -5,6 +5,6 @@ def qsort(l):
         pivot = l[0]
         left = qsort([x for x in l if x < pivot])
         right = qsort([x for x in l if x >pivot])
-        return left+[pivot]+right
+        return left + [pivot] + right
 
 #print(qsort([3,2,1]))
